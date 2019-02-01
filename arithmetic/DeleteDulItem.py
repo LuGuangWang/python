09f5bd173caa8692474a4302996a81e59b55d9a9
wangluguang <wangluguang@jd.com>
2019-02-01 19:07:36 +0800

    add

---
+++ arithmetic/DeleteDulItem.py
@@ -0,0 +1,62 @@
+#[删除链表中重复的结点]
+#在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留，返回链表头指针。
+#例如，链表1->1->2->3->3->4->4->5->5
+#处理后为
+#2->5
+class Node(object):
+
+    def __init__(self,x):
+        self.val = x
+        self.next = None
+
+    def sprint(self):
+        tmp = self
+        while tmp is not None:
+            print(tmp.val)
+            tmp = tmp.next
+        print('\n')
+
+def delDulp(list):
+    head=last=None
+    tmp = list
+    while tmp is not None:
+        flag = False
+        next = tmp.next
+
+        while (next is not None):
+            if (tmp.val == next.val):
+                next = next.next
+                flag = True
+            elif flag:
+                tmp = next
+                next = tmp.next
+                flag = False
+            else:
+                break
+        if flag:
+            tmp = next
+
+        if head is None:
+            head = tmp
+            last = tmp
+        else:
+            last.next = tmp
+            last = last.next
+
+        tmp=next
+    return head
+
+#1->1->2->3->3->4->4->5->6->6
+list = Node(1)
+list.next=Node(1)
+list.next.next=Node(2)
+list.next.next.next=Node(3)
+list.next.next.next.next=Node(3)
+list.next.next.next.next.next=Node(4)
+list.next.next.next.next.next.next=Node(4)
+list.next.next.next.next.next.next.next=Node(5)
+list.next.next.next.next.next.next.next.next=Node(6)
+list.next.next.next.next.next.next.next.next.next=Node(6)
+
+res = delDulp(list)
+res.sprint()
